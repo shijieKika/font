b11d2744c53dd66f5ab8e30527b7fd35a9e853f2
Mashijie <shijie.ma@kikatech.com>
2017-09-14 21:01:02 +0800

    add arg parser in classify_image

--- src/utils/classify_image.py
+++ src/utils/classify_image.py
@@ -4,8 +4,10 @@ import os
 import re
 import random
 import shutil
+import argparse
 
 file_name = re.compile(r"uni.*_.*\.png")
+FLAGS = None
 
 
 def traversal_data(src_path, train_path, test_path, radio=0.1):
@@ -59,19 +61,22 @@ def filter_data(src_path, radio=0.1):
 
 
 def main():
-    traversal_data('/Users/msj/Code/font/data/debug_data/negative_data',
-                   '/Users/msj/Code/font/data/debug_data/negative_train',
-                   '/Users/msj/Code/font/data/debug_data/negative_valid')
-
-    # path1 = "../../data/debug_data/full_data"
-    # path2 = '/Users/msj/Code/font/data/debug_data/negative_data'
-    # filter_data(path2)
+    # traversal_data('/Users/msj/Code/font/data/debug_data/negative_data',
+    #                '/Users/msj/Code/font/data/debug_data/negative_train',
+    #                '/Users/msj/Code/font/data/debug_data/negative_valid')
 
     # traversal_data('/home/mashijie/data/training_data/negative_data',
     #                '/home/mashijie/data/training_data/negative_train', '../../data/debug_data/negative_valid')
 
-    pass
+    traversal_data(FLAGS.data_dir, FLAGS.train_dir, FLAGS.test_dir, FLAGS.radio)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", help="Need data dir")
+    parser.add_argument("--train_dir", help="Need train dir")
+    parser.add_argument("--test_dir", help="Need test dir")
+    parser.add_argument("--radio", type=float, default=0.1)
+
+    FLAGS = parser.parse_args()
     main()
