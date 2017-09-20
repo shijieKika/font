384e449bb20ba4a95f57f72bb7a1e91b4579b45d
Mashijie <shijie.ma@kikatech.com>
2017-09-20 10:47:54 +0800

    choice device

--- inference.sh
+++ inference.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python2 -u src/main.py --gpu --inference \
-                             --data_dir "/mnt/mashijie/data/train_positive" \
-                             --right_dir "/mnt/mashijie/inference/right_data" \
-                             --wrong_dir "/mnt/mashijie/inference/wrong_data" \
-                             --checkpoint_dir "/mnt/mashijie/checkpoint" \
-                             --chinese_dict_dir "./static/chinese.dict"
+python2 -u src/main.py --inference --gpu --device 0 --gpu_fraction 0.5\
+                                   --data_dir "/mnt/mashijie/data/train_positive" \
+                                   --right_dir "/mnt/mashijie/inference/right_data" \
+                                   --wrong_dir "/mnt/mashijie/inference/wrong_data" \
+                                   --checkpoint_dir "/mnt/mashijie/checkpoint" \
+                                   --chinese_dict_dir "./static/chinese.dict"
--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nohup python2 -u src/main.py --gpu --training \
-                                   --data_dir "/mnt/mashijie/data/train_positive" \
-                                   --valid_positive_dir "/mnt/mashijie/data/valid_positive" \
-                                   --valid_negative_dir "/mnt/data/training_data/negative_data" \
-                                   --checkpoint_dir "/mnt/mashijie/checkpoint" \
-                                   --chinese_dict_dir "./static/chinese.dict" \
-                                   > /mnt/mashijie/train.log &
+nohup python2 -u src/main.py --training --gpu --device 0 --gpu_fraction 0.5\
+                                        --data_dir "/mnt/mashijie/data/train_positive" \
+                                        --valid_positive_dir "/mnt/mashijie/data/valid_positive" \
+                                        --valid_negative_dir "/mnt/data/training_data/negative_data" \
+                                        --checkpoint_dir "/mnt/mashijie/checkpoint" \
+                                        --chinese_dict_dir "./static/chinese.dict" \
+                                        > /mnt/mashijie/train.log &
